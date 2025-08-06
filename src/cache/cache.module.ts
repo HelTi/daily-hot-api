7e510e8f97533d4a01bb170e8ed1b1db2fbbb42a
helti <helti@qq.com>
2025-08-06 22:01:55 +0800

    feat(cache): 🐳 增加内存缓存，没有redis也可以正常运行

--- src/cache/cache.module.ts
+++ src/cache/cache.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { CacheService } from './cache.service';
 import { RedisModule as NestRedisModule } from '@nestjs-modules/ioredis';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CacheModule as NestCacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
@@ -14,11 +15,22 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
           host: configService.get('REDIS_HOST'),
           port: configService.get('REDIS_PORT'),
           password: configService.get('REDIS_PASSWORD'),
+          lazyConnect: true, // 启动不握手
+          maxRetriesPerRequest: 1,
+          enableReadyCheck: false,
+          retryStrategy: () => null,
         },
         onClientReady: (client) => {
           console.log('Redis client ready', client);
         },
+        onClientError: (error) => {
+          console.error('Redis client error', error);
+        },
       }),
+    }),
+    NestCacheModule.register({
+      ttl: 0, // 默认禁用全局TTL（由业务代码控制）
+      max: 1000, // 内存缓存最大项目数
     }),
   ],
   providers: [CacheService],
