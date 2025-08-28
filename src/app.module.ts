46a3d68b3242ec7f4b01a84e7557ed0d849d68da
helti <helti@qq.com>
2025-08-28 17:07:16 +0800

    feat(crawler): Add scheduled hot info crawling task 🚀

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,22 @@ import { AppService } from './app.service';
 import { CacheModule } from './cache/cache.module';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 import { AppConfigModule } from './config/config.module';
+import { DatabaseModule } from './database/database.module';
 import { HotListsModule } from './host-lists/hot-lists.module';
+import { HistoryModule } from './history/history.module';
+import { SchedulerModule } from './scheduler/scheduler.module';
 import { TokenModule } from './token/token.module';
 
 @Module({
-  imports: [CacheModule, AppConfigModule, HotListsModule, TokenModule],
+  imports: [
+    CacheModule,
+    AppConfigModule,
+    DatabaseModule,
+    HotListsModule,
+    HistoryModule,
+    SchedulerModule,
+    TokenModule,
+  ],
   controllers: [AppController],
   providers: [AppService, LoggerMiddleware],
 })
